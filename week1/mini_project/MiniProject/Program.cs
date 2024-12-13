44cd6cc91448ff448447f8343566860afacac69a
Ali Alrubaye <alialrubaye1990@hotmail.com>
2024-12-12 22:12:20 -0500

    program runs as intended

--- week1/mini_project/MiniProject/Program.cs
+++ week1/mini_project/MiniProject/Program.cs
@@ -5,20 +5,14 @@ class Program
     static void Main(string[] args)
     {
          
-        // foreach (Car c in DummyData.GetCars()){
-        //     Console.WriteLine(c);
-        // }
-
         while (true){
 
             Console.WriteLine("------------------------------------------------------\n"+
-                            "Welcome. Please select from 1-6:\n"+
+                            "Welcome. Please select from 1-4:\n"+
                             "1) Show all inventory\n"+
                             "2) Search inventory by make\n"+
-                            "3) Search inventory by model\n"+
-                            "4) Search inventory by year\n"+
-                            "5) Show EV (Electric Vehicles) only\n"+
-                            "6) Exit\n------------------------------------------------------");
+                            "3) Show EV (Electric Vehicles) only\n"+
+                            "4) Exit\n------------------------------------------------------");
             string? userInput = Console.ReadLine();
 
 
@@ -27,38 +21,59 @@ class Program
             {
                 switch (int.Parse(userInput))
             {
-                case 1:                
+                case 1:  
+                    foreach (Car c in DummyData.GetCars()){
+                      Console.WriteLine(c);
+                    }              
                 break;
 
                 case 2:
+                    Console.WriteLine("Select car make (Toyota, Honda, Nissan, or Tesla)");
+                    string? userInputMake = Console.ReadLine();
+                    
+                    if(userInputMake.ToLower().Equals("toyota")){
+                         foreach (Car c in DummyData.GetCars()){
+                         if(c.Make.Equals("Toyota")) Console.WriteLine(c);
+                         } 
+                    } else if(userInputMake.ToLower().Equals("honda")){
+                        foreach (Car c in DummyData.GetCars()){
+                         if(c.Make.Equals("Honda")) Console.WriteLine(c);
+                         }
+                    } else if(userInputMake.ToLower().Equals("nissan")){
+                        foreach (Car c in DummyData.GetCars()){
+                         if(c.Make.Equals("Nissan")) Console.WriteLine(c);
+                         }
+                    } else if(userInputMake.ToLower().Equals("tesla")){
+                        foreach (Car c in DummyData.GetCars()){
+                         if(c.Make.Equals("Tesla")) Console.WriteLine(c);
+                         }
+                    } else {
+                        Console.WriteLine("Invalid input!");
+                    }                    
+                break;
+              
+                case 3:
+                    foreach (Car c in DummyData.GetCars()){
+                      if(c.Ev) Console.WriteLine(c);
+                    } 
                 break;
 
-                case 3:
+                case 4:
+                Console.WriteLine("Thank you!");
+                Environment.Exit(0);
                 break;
+
 
                 //invalid integer
                 default:
+                Console.WriteLine("Invalid input!");
                 break;
             }
             }
             else
             {
-                Console.WriteLine("invalid input!");
+                Console.WriteLine("Invalid input!");
             }
-
-
-
-
-            
-
-
-
-
-
-
-
-
-
 
 
 
